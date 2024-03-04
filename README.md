6ad69d3839bc8baa4112de9741c2a56226d0651c
tharunsuresh-code <stharunvikram@gmail.com>
2024-03-03 19:53:18 -0800

    Added docstring to trie and update readme with basic details

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# python-trie
-A basic implementation of word dictionary using the Trie data structure
+# pytrie
+A basic implementation of word dictionary using the Trie data structure in Python 3.
+
+A trie (derived from retrieval) is a multiway tree data structure 
+used for storing strings over an alphabet. It is used to store a large amount 
+of strings. The pattern matching can be done efficiently using tries.
+
+To clone the repo:
+```bash
+git clone https://github.com/tharunsuresh-code/pytrie.git
+```
+
+## Usage
+Create a new ``Trie`` to use as a word dictionary:
+```python
+from pytrie import Trie
+
+trie = Trie()
+trie.addWord("hello")
+trie.addWord("world")
+trie.addWord("Harry")
+trie.addWord("Potter")
+```
+
+Now you can query the Trie for any string:
+```python
+>>> trie.search("hello")
+True
+>>> trie.search("world")
+True
+>>> trie.search("harry")
+False
+>>> trie.search("Harry")
+True
+```
+
+## Author and References
+Docs are inspired from [PyTrie](https://github.com/gsakkis/pytrie) and [datrie](https://github.com/pytries/datrie).
+
+## License
+Licensed under MIT.
--- pytrie.py
+++ pytrie.py
@@ -1,5 +1,46 @@
 from collections import defaultdict
 
+"""
+Definition: A trie (derived from retrieval) is a multiway tree data structure 
+used for storing strings over an alphabet. It is used to store a large amount 
+of strings. The pattern matching can be done efficiently using tries.
+
+The trie shows words like allot, alone, ant, and, are, bat, bad. The idea is 
+that all strings sharing common prefix should come from a common node. The 
+tries are used in spell checking programs.
+
+Preprocessing pattern improves the performance of pattern matching algorithm. 
+But if a text is very large then it is better to preprocess text instead of 
+pattern for efficient search.
+A trie is a data structure that supports pattern matching queries in time 
+proportional to the pattern size.
+
+Ref: https://www.geeksforgeeks.org/trie-insert-and-search/
+
+Usage
+-----
+import pytrie
+
+>>> trie = pytrie.Trie()
+>>> trie.addWord("hello")
+>>> trie.addWord("world")
+>>> trie.addWord("worlds")
+>>> trie.addWord("Harry")
+>>> trie.addWord("Potter")
+
+>>> trie
+<pytrie.Trie at 0x7fb17466bc70>
+
+>>> trie.search("hello")
+True
+
+>>> trie.search("harry")
+False
+
+>>> trie.search("Harry")
+True
+"""
+
 
 class Trie:
 
